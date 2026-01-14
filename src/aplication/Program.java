aaf88310900ff19db215b17a6ee4c22f64cf289e
Junior Lopes <juniorschnayder1@gmail.com>
2026-01-13 22:40:18 -0300

    Created implements interface SellerDaoJDBC and DepartmentDaoJDBC, Created class DaoFactory

---
+++ src/aplication/Program.java
@@ -0,0 +1,23 @@
+package aplication;
+
+
+import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department department = new Department(1,"Math");
+		Seller seller = new Seller(2,"Junior","junior@gmail.com",new Date(),3000.0,department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(seller);
+	}
+
+}
