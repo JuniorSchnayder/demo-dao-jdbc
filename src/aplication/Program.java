637758707d2e37cdb8fecae9cdb2a56984d57701
Junior Lopes <juniorschnayder1@gmail.com>
2026-01-14 22:10:15 -0300

    Reusing instantiation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -16,13 +16,18 @@ public class Program {
 		Department department = new Department(1,"Math");
 		Seller seller = new Seller(2,"Junior","junior@gmail.com",new Date(),3000.0,department);
 		
+		System.out.println("===== TEST 1: seller findById =====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(sellerDao.findById(3));
-		
+		System.out.println();
+
+		System.out.println("===== TEST 2: seller findAll =====");
 		List<Seller> seller2 = sellerDao.findAll();
 		for(Seller s : seller2) {
 			System.out.println(s);
 		}
+		System.out.println();
+
 	}
 
 }
