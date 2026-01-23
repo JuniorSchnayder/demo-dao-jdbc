d617c9fa4f7c3c3fb1f36f7012ef0fea91a6df9d
Junior Lopes <juniorschnayder1@gmail.com>
2026-01-22 21:33:12 -0300

    refactor: simplify conditional logic using switch

--- src/aplication/Program2.java
+++ src/aplication/Program2.java
@@ -13,45 +13,73 @@ public class Program2 {
 		Scanner sc = new Scanner(System.in);
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		/*
-		 * System.out.
-		 * println("============================TESTE #1 insert==============================="
-		 * ); System.out.print("Digite o nome do departamento: "); String nome =
-		 * sc.next(); Department dep = new Department(null,nome);
-		 * departmentDao.insert(dep);
-		 */
+		int opcao;
+		do {
+			System.out.println("\n==================== MENU ====================");
+			System.out.println("1 - Insert");
+			System.out.println("2 - Update");
+			System.out.println("3 - Delete by Id");
+			System.out.println("4 - Find by Id");
+			System.out.println("5 - Find All");
+			System.out.println("0 - Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = sc.nextInt();
+			sc.nextLine();
 
-		/*
-		 * System.out.
-		 * println("============================TESTE #2 update==============================="
-		 * ); System.out.print("Digite o Id do departamento que desejar alterar: "); int
-		 * id = sc.nextInt(); sc.nextLine();
-		 * System.out.print("Digite o novo nome do departamento: "); String nomeUpdate =
-		 * sc.next(); Department depUpdate = new Department(id,nomeUpdate);
-		 * departmentDao.update(depUpdate);
-		 */
+			switch (opcao) {
+			case 1:
+				System.out.println("============================TESTE #1 insert===============================");
+				System.out.print("Digite o nome do departamento: ");
+				String nome = sc.next();
+				Department dep = new Department(null, nome);
+				departmentDao.insert(dep);
+				break;
+				
+			case 2:
+				System.out.println("============================TESTE #2 update===============================");
+				System.out.print("Digite o Id do departamento que desejar alterar: ");
+				int id = sc.nextInt();
+				sc.nextLine();
+				System.out.print("Digite o novo nome do departamento: ");
+				String nomeUpdate = sc.next();
+				Department depUpdate = new Department(id, nomeUpdate);
+				departmentDao.update(depUpdate);
+				break;
+				
+			case 3:
+				System.out.println("============================TESTE #3 deleteById===============================");
+				System.out.print("Digite o Id do departamento que desejar deletar: ");
+				int idDelete = sc.nextInt();
+				departmentDao.deleteById(idDelete);
+				break;
+				
+			case 4:
+				System.out.println("============================TESTE #4 findById===============================");
+				System.out.print("Digite o Id do departamento: ");
+				int idFind = sc.nextInt();
+				Department find = departmentDao.findById(idFind);
+				System.out.println(find);
+				break;
+				
+			case 5:
+				System.out.println("============================TESTE #5 findAll===============================");
+				List<Department> findAll = departmentDao.findAll();
+				for (Department d : findAll) {
+					System.out.println(d);
+				}
+				break;
+				
+			case 0:
+				System.out.println("Encerrando...");
+				break;
+			
+			default:
+				System.out.println("Opção inválida!");
 
-		/*
-		 * System.out.
-		 * println("============================TESTE #3 deleteById==============================="
-		 * ); System.out.print("Digite o Id do departamento que desejar deletar: "); int
-		 * idDelete = sc.nextInt(); departmentDao.deleteById(idDelete);
-		 */
+			}
 
-		/*
-		 * System.out.
-		 * println("============================TESTE #4 findById==============================="
-		 * ); System.out.print("Digite o Id do departamento: "); int idFind =
-		 * sc.nextInt(); Department find = departmentDao.findById(idFind);
-		 * System.out.println(find);
-		 */
-
-		System.out.println("============================TESTE #5 findAll===============================");
-		List<Department> findAll = departmentDao.findAll();
-		for (Department d : findAll) {
-			System.out.println(d);
-		}
-
+		} while (opcao != 0);
+		sc.close();
 	}
 
 }
